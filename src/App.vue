2e7f0c5b350755a456a0e92ad6e3534830268331
Carlos Diaz <cdiaz86@gmail.com>
2020-02-05 18:11:03 -0500

    Adds initial files & Title component.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PageTitle title="How long until"/> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageTitle from './components/Title.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    PageTitle,
+  },
 }
 </script>
 
---
+++ src/components/Title.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="title">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageTitle',
+  props: {
+    title: String,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-size: 50px;
+  color: rgb(0, 0, 0);
+}
+
+</style>
