9f399308441cb30c2743fc19d96fc8029fe46522
Carlos Diaz <cdiaz86@gmail.com>
2020-02-23 13:03:13 -0500

    Adds language to results.js.

--- src/json_files/result.js
+++ src/json_files/result.js
@@ -1,6 +1,6 @@
 const result = 
 {
-  intro: "I know what you are thinking... Where is the timeline? Here is the hard truth: estimating exactly when you'll know enough to get a job is really, really hard. Learning to code is not like baking a cake. You can't set a timer and wait for that coding cake to come out the oven. There are so many variables to learning: your level of prior experience, the depth of that prior experience, time available for learning, efficiency in that available time, rate at which you learn concepts of varying difficulty, rate at which you work through projects of varying difficulty, etc, etc, etc...",
+  intro: "I know what you are thinking... Where is the timeline? Here is the hard truth: estimating exactly when you'll know enough to get a job is really, really hard. Learning to code is not like baking a cake. You can't set a timer and wait for that coding cake to come out the oven. There are so many variables to learning: your level of prior experience, the depth of that prior experience, time available for learning, efficiency in that available time, rate at which you learn concepts of varying difficulty, rate at which you work through projects of varying difficulty, etc, etc, etc... It's better to focus on learning well than it is to focus on learning fast.",
   tips: {
     tip1: "Pick one learning resource and stick with it",
     tip2: "Find a community of learners.",
